20d147f1d084ed17bfeebbfb6ddfa7d68846f812
vagrant <vagrant@homestead>
2021-06-18 02:43:16 +0000

    登录成功后导航逻辑

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -15,7 +15,7 @@ class SessionsController extends Controller
             'email' => 'required|email|max:255',
             'password' => 'required'
         ]);
-        dump($credentials);
+        //dump($credentials);
         if(Auth::attempt($credentials)){
             //登录成功
             session()->flash('success', '欢迎回来！');
